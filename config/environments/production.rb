958ab379db1a2bbc6f1953eab0cd72004de64a57
sfurmb <rmb.sfu@gmail.com>
2016-07-28 17:23:41 -0700

    test: adding static images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   # zqian
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # zqian
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
